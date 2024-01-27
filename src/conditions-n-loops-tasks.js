3ce8ef79e438b49a445313937cfba77dcb90a4a7
Olga Zhuravleva <onpol@mail.ru>
2024-01-27 19:19:44 +0300

    feat: completed 6 tasks out of 19

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -21,8 +21,8 @@
  *  0  => true
  *  -5 => false
  */
-function isPositive(/* number */) {
-  throw new Error('Not implemented');
+function isPositive(number) {
+  return number >= 0;
 }
 
 /**
@@ -38,8 +38,10 @@ function isPositive(/* number */) {
  *  -5, 0, 5      => 5
  *  -0.1, 0, 0.2  => 0.2
  */
-function getMaxNumber(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getMaxNumber(a, b, c) {
+  if (a >= b && a >= c) return a;
+  if (b >= a && b >= c) return b;
+  return c;
 }
 
 /**
@@ -60,8 +62,14 @@ function getMaxNumber(/* a, b, c */) {
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  */
-function canQueenCaptureKing(/* queen, king */) {
-  throw new Error('Not implemented');
+function canQueenCaptureKing(queen, king) {
+  if (
+    queen.x === king.x ||
+    queen.y === king.y ||
+    Math.abs(queen.x - king.x) === Math.abs(queen.y - king.y)
+  )
+    return true;
+  return false;
 }
 
 /**
@@ -82,8 +90,10 @@ function canQueenCaptureKing(/* queen, king */) {
  *  2, 2, 5   => false
  *  3, 0, 3   => false
  */
-function isIsoscelesTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isIsoscelesTriangle(a, b, c) {
+  if (a + b > c && b + c > a && a + c > b && (a === b || b === c || a === c))
+    return true;
+  return false;
 }
 
 /**
@@ -100,8 +110,41 @@ function isIsoscelesTriangle(/* a, b, c */) {
  *  10  => X
  *  26  => XXVI
  */
-function convertToRomanNumerals(/* num */) {
-  throw new Error('Not implemented');
+function convertToRomanNumerals(num) {
+  let str = '';
+  for (let i = 0; i < Math.floor(num / 10); i += 1) str += 'X';
+  switch (num % 10) {
+    case 1:
+      str += 'I';
+      break;
+    case 2:
+      str += 'II';
+      break;
+    case 3:
+      str += 'III';
+      break;
+    case 4:
+      str += 'IV';
+      break;
+    case 5:
+      str += 'V';
+      break;
+    case 6:
+      str += 'VI';
+      break;
+    case 7:
+      str += 'VII';
+      break;
+    case 8:
+      str += 'VIII';
+      break;
+    case 9:
+      str += 'IX';
+      break;
+    default:
+      break;
+  }
+  return str;
 }
 
 /**
@@ -135,8 +178,10 @@ function convertNumberToString(/* numberStr */) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  let palindrome = '';
+  for (let i = 0; i < str.length; i += 1) palindrome = str[i] + palindrome;
+  return str === palindrome;
 }
 
 /**
